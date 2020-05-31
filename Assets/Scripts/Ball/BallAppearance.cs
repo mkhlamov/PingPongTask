2666065ca7b67586c4bd4dad6a391011e5034285
mkhlamov <mkhlamov@gmail.com>
2020-05-31 15:27:04 +0300

    Add BallAppearance class. Implement SetColor

---
+++ Assets/Scripts/Ball/BallAppearance.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PingPongTask.Ball
+{
+    public class BallAppearance
+    {
+        private Image _image;
+
+        public BallAppearance(Image image)
+        {
+            _image = image;
+        }
+
+        public void SetColor(Color color)
+        {
+            _image.color = color;
+        }
+        
+        
+    }
+}
---
+++ Assets/Tests/EditMode/BallAppearanceTests/BallAppearanceTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PingPongTask.Ball;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tests.EditMode.BallAppearanceTests
+{
+    public class BallAppearanceTests
+    {
+        public class BallAppearanceSetColor : BallAppearanceTests
+        {
+            [Test]
+            public void _0_Should_Change_Color()
+            {
+                var image = new GameObject().AddComponent<Image>();
+                image.color = Color.black;
+                var appearance = new BallAppearance(image);
+                var expectedColor = Color.green;
+
+                appearance.SetColor(expectedColor);
+
+                Assert.AreEqual(expectedColor, image.color);
+            }
+        }
+    }
+}
